8bae84532ca47c1815084f92790d1750135f47d7
matheusbritodev <matheusbritodev1@gmail.com>
2026-08-15 20:00:31 -0300

    refactor(item): remove estado interno precoTotal e isola regra de calculo

--- desafios-diarios/dia34/domain/item.js
+++ desafios-diarios/dia34/domain/item.js
@@ -1,25 +1,21 @@
-
-
-
 export class Item {
-    constructor(id, nome, precoBase, categoria) {
+    constructor(id, nome, quantidade, precoBase, categoria, nomeDoAdicional=null, precoAdicional=null) {
+        this.id = id
         this.nome = nome
+        this.quantidade = quantidade
         this.precoBase = precoBase
         this.categoria = categoria
-    }
-}
-
-export class Acompanhamento extends Item {
-    constructor(id, nome, precoBase, categoria) {
-        super(id, nome, precoBase, categoria)
-    }
-}
-
-export class Bebida extends Item {
-    constructor(id, nome, precoBase, categoria, nomeDoAdicional, precoAdicional) {
-        super(id, nome, precoBase, categoria)
         this.nomeDoAdicional = nomeDoAdicional
         this.precoAdicional = precoAdicional
+        this.precoTotal = 0
+    }
+
+    calcularPrecoTotal() {
+        if (this.precoAdicional === null) {
+            return this.precoBase * this.quantidade + 0
+
+        } else {
+            return this.precoBase * this.quantidade + this.precoAdicional
+        }
     }
 }
-
