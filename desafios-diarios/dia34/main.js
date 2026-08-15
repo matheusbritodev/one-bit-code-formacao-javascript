b73c40a8cadef7b6deb3802a3a358ea7b74b2afb
matheusbritodev <matheusbritodev1@gmail.com>
2026-08-15 20:01:34 -0300

    refactor(main): move responsabilidade de exibicao de dados para o arquivo principal

--- desafios-diarios/dia34/main.js
+++ desafios-diarios/dia34/main.js
@@ -1,6 +1,25 @@
-import { Cardapio } from "./domain/cardapio.js";
 import { Cliente } from "./domain/cliente.js";
-import { Item, Bebida, Acompanhamento } from "./domain/item";
+import { Item } from "./domain/item.js";
 import { Pedido } from "./domain/pedido.js";
 
 
+const cliente1 = new Cliente(1, "Matheus")
+const pedido1 = new Pedido(1, cliente1)
+const cliente2 = new Cliente(2, "Brito")
+const pedido2 = new Pedido(2, cliente2)
+
+const acompanhamento1 = new Item(1, "bolo", 1, 10.00, "acompanhamento")
+const bebida1 = new Item(2, "refrigerante", 1, 12.00, "bebida")
+
+console.log(pedido1.adicionarItem(acompanhamento1))
+console.log(pedido1.adicionarItem(bebida1))
+
+console.table(pedido1.visualizarPedidos())
+
+console.log(cliente1.fecharConta(pedido1))
+
+console.log(pedido1.removerItem(bebida1))
+
+console.log(cliente1.fecharConta(pedido1))
+console.log(pedido1.adicionarItem(bebida1))
+console.log(cliente1.fecharConta(pedido1))
