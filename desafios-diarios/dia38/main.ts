2fc4d23455176d07d2cf179af22e6bf0e6ac6a9b
matheusbritodev <matheusbritodev1@gmail.com>
2026-08-20 17:43:10 -0300

    feat: implements game logic, creates control variables for players, changes parameters to a more appropriate name

--- desafios-diarios/dia38/main.ts
+++ desafios-diarios/dia38/main.ts
@@ -1,5 +1,26 @@
-function jokenpo(jogador1: String, jogador2: String) {
-    const winner: Number = 0
-    const validEntry: Array<String> = ["Pedra", "Papel", "Tesoura"]
+function jokenpo(p1: string, p2: string) {
+    let winner: number = 0
+    const validEntry: Array<string> = ["rock", "paper", "scissors"]
 
+    const player1: string = p1.toLowerCase()
+    const player2: string = p2.toLowerCase()
+
+    if (validEntry.includes(player1) && validEntry.includes(player2)) {
+        if (player1 === player2) {
+            return winner
+        } else if (player1 === "rock" && player2 === "scissors") {
+            return winner = 1
+        } else if (player1 === "scissors" && player2 === "paper") {
+            return winner = 1
+        } else if (player1 === "paper" && player2 === "rock") {
+            return winner = 1
+        } else {
+            return winner = 2
+        }
+    } else {
+        return winner = -1
+    }
 }
+
+
+
